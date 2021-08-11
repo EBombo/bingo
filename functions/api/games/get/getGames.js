8008b87df7eaedceb679592c8a3660048104d622
Pablo <pablo.yncio@immertec.com>
2021-08-10 23:47:40 -0500

    fetch games from database

---
+++ functions/api/games/get/getGames.js
@@ -0,0 +1,30 @@
+const logger = require("../../../utils/logger");
+const { snapshotToArray } = require("../../../utils");
+const { firestore } = require("../../../config");
+
+exports.getGames = async (req, res, next) => {
+  try {
+    logger.log("getGames->", req.params);
+
+    const { userId } = req.params;
+    const { folderId } = req.query;
+
+    let gamesRef = firestore
+      .collection("games")
+      .where("usersIds", "array-contains", userId)
+      .where("deleted", "==", false);
+
+    gamesRef = folderId
+      ? gamesRef.where("parent.id", "==", folderId)
+      : gamesRef.where("parent", "==", null);
+
+    const gamesQuery = await gamesRef.get();
+
+    const games = snapshotToArray(gamesQuery);
+
+    return res.send(games);
+  } catch (error) {
+    logger.error(error);
+    next(error);
+  }
+};
