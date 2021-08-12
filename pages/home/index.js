e6ee18c1936c9f46b3c316f2f5f072e67ce36e60
Pablo <pablo.yncio@immertec.com>
2021-08-12 17:27:19 -0500

    ...

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,23 +1,13 @@
 import React from "reactn";
-import {Home} from "../../src/pages/home";
-import dynamic from "next/dynamic";
-import {spinLoader} from "../../src/components/common/loader";
-import {SEOMeta} from "../../src/components/common/seo";
-import {UserPrivateRoute} from "../../src/routes/UserPrivateRoute";
+import { Home } from "../../src/pages/home";
+import { SEOMeta } from "../../src/components/common/seo";
+import { UserPrivateRoute } from "../../src/routes/UserPrivateRoute";
 
-const UserLayout = dynamic(() => import("../../src/components/UserLayout"),
-    {
-        ssr: false,
-        loading: () => spinLoader()
-    }
+const HomePage = (props) => (
+  <UserPrivateRoute>
+    <SEOMeta {...props} />
+    <Home {...props} />
+  </UserPrivateRoute>
 );
-
-const HomePage = props =>
-    <UserPrivateRoute>
-        <SEOMeta {...props}/>
-        <UserLayout {...props}>
-            <Home {...props}/>
-        </UserLayout>
-    </UserPrivateRoute>
 
 export default HomePage;
