0b51b3bff6b3fbb093aaf8d5fce88775685289f7
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-08-19 10:34:15 -0500

    fixing update

--- functions/api/games/put/putGame.js
+++ functions/api/games/put/putGame.js
@@ -3,21 +3,18 @@ const { firestore } = require("../../../config");
 
 exports.putGame = async (req, res, next) => {
   try {
-    logger.log("getGame->", req.params, req.query, req.body);
+    logger.log("putGame->", req.params, req.query, req.body);
 
     const { userId, gameId } = req.params;
     const { folderId } = req.query;
     const game = req.body;
 
-    await firestore
-      .collection("games")
-      .doc(gameId)
-      .update({
-        ...game,
-        usersIds: [userId],
-        parentId: folderId || null,
-        updateAt: new Date(),
-      });
+    await firestore.doc(`games/${gameId}`).update({
+      ...game,
+      usersIds: [userId],
+      parentId: folderId || null,
+      updateAt: new Date(),
+    });
 
     res.send({ success: true });
   } catch (error) {
