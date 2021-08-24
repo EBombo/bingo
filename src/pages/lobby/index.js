7cf7168a1abd6f53eceb34ebfa6ebb0eafd5510b
Pablo <pablo.yncio@immertec.com>
2021-08-24 00:45:02 -0500

    fetch user by token to verify account

--- src/pages/lobby/index.js
+++ src/pages/lobby/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "reactn";
 import styled from "styled-components";
 import { useRouter } from "next/router";
-import { config, firestore } from "../../firebase";
+import { authEvents, config, firestore } from "../../firebase";
 import { useFetch } from "../../hooks/useFetch";
+import { ButtonBingo } from "../../components/form";
 
 export const Lobby = () => {
   const { Fetch } = useFetch();
@@ -13,19 +14,17 @@ export const Lobby = () => {
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    if (!tokenId && !gameId) return router.push("/login");
+    if (!tokenId || !gameId) return;
 
-    const fetchUserByToken = async (tokenId) => {
+    const fetchUserByToken = async () => {
       try {
-        const url = `${config.serverUrlEvents}/api/tokens/${tokenId}`;
-
-        const { response, error } = await Fetch(url);
+        const url = `${config.serverUrlEvents}/api/tokens`;
+        const { response, error } = await Fetch(url, "POST", { tokenId });
 
         if (error) return router.push("/login");
 
         const authUser = response.user;
 
-        console.log("authUser", authUser);
         if (!authUser) return router.push("/login");
 
         const gameRef = await firestore.doc(`games/${gameId}`).get();
@@ -37,23 +36,60 @@ export const Lobby = () => {
         setIsAdmin(true);
         setIsLoading(false);
         await router.push({
-          pathname: router.asPath,
+          pathname: router.asPath.split("?")[0],
         });
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchUserByToken(tokenId);
+    fetchUserByToken();
   }, [tokenId, gameId]);
+
+  if (isLoading) return "spin";
 
   return (
     <LobbyCss>
-      Lobby {String(isLoading)} {game?.name}
+      <div>
+        <ButtonBingo variant="primary" width="100%">
+          Día del padre
+        </ButtonBingo>
+        <div className="container-lobby">
+          <div className="item">
+            <div>Jugadores vs Jugadores</div>
+            <div>1:1 dispositivos</div>
+            <ButtonBingo>Clásico</ButtonBingo>
+          </div>
+          <div className="item">
+            <div>Jugadores vs Jugadores</div>
+            <div>1:1 dispositivos</div>
+            <ButtonBingo>Clásico</ButtonBingo>
+          </div>
+        </div>
+        <ButtonBingo variant="primary" width="100%">
+          Opciones del juego
+        </ButtonBingo>
+      </div>
     </LobbyCss>
   );
 };
 
 const LobbyCss = styled.div`
+  width: 100%;
+  margin: auto;
+  max-width: 400px;
   color: ${(props) => props.theme.basic.white};
+
+  .container-lobby {
+    grid-gap: 5px;
+    padding: 10px 5px 5px 5px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    .item {
+      background: gray;
+      border-radius: 4px;
+      text-align: center;
+    }
+  }
 `;
