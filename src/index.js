f0a4586f0e2077569ef424ebd8758ef65d629b3c
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-09-12 23:21:05 -0500

    add Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
 const logger = require("../functions/utils/logger");
 const bodyParser = require("body-parser");
-const {default: next} = require("next");
+const { default: next } = require("next");
 const express = require("express");
 const cors = require("cors");
 
 const isDev = process.env.NODE_ENV !== "production";
 
 const server = next({
-    dev: isDev,
-    conf: {distDir: ".next"},
+  dev: isDev,
+  conf: { distDir: ".next" },
 });
 
 const nextHandle = server.getRequestHandler();
 
 const serverExpress = express();
 
-serverExpress.use(cors({origin: "*"}));
+serverExpress.use(cors({ origin: "*" }));
 
 serverExpress.use(bodyParser.json());
 
-serverExpress.use(bodyParser.urlencoded({extended: false}));
+serverExpress.use(bodyParser.urlencoded({ extended: false }));
 
-serverExpress
-    .all("*", async (req, res, next) => {
-        try {
-            await server.prepare();
+serverExpress.all("*", async (req, res, next) => {
+  try {
+    await server.prepare();
 
-            await nextHandle(req, res);
-        } catch (error) {
-            logger.log("error", error);
-            next(error);
-        }
-    });
+    await nextHandle(req, res);
+  } catch (error) {
+    logger.log("error", error);
+    next(error);
+  }
+});
 
-module.exports = {serverExpress};
+module.exports = { serverExpress };
