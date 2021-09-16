6a73eae7405336cdcf6d1d9d9fae840a0579d6ae
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-09-16 11:25:07 -0500

    ignore function redirect hosting[front] to cloud run

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
 const index = require("firebase-functions");
 const api = require("./api");
-const {serverExpress} = require("../src");
 
 const runtimeOptions = {
-    timeoutSeconds: 60,
-    memory: "256MB",
+  timeoutSeconds: 60,
+  memory: "256MB",
 };
 
 const apiRegion = "us-central1";
@@ -12,11 +11,4 @@ const apiRegion = "us-central1";
 exports.api = index
     .runWith(runtimeOptions)
     .region(apiRegion)
-    .https
-    .onRequest(api.api);
-
-exports.next = index
-    .runWith(runtimeOptions)
-    .region(apiRegion)
-    .https
-    .onRequest(serverExpress);
+    .https.onRequest(api.api);
