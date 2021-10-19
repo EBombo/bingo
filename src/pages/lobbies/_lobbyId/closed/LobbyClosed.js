b2c9a0ea94f86f9ae119ffa313efb264294fbbbb
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-10-19 17:59:57 -0500

    report admin

--- src/pages/lobbies/_lobbyId/closed/LobbyClosed.js
+++ src/pages/lobbies/_lobbyId/closed/LobbyClosed.js
@@ -1,45 +1,134 @@
-import { fadeOutUpBig } from "react-animations";
 import React, { useEffect, useState } from "reactn";
 import styled, { keyframes } from "styled-components";
 import { timeoutPromise } from "../../../../utils/promised";
 import { mediaQuery } from "../../../../constants";
 import { Winner } from "./Winner";
+import { ButtonAnt, ButtonBingo } from "../../../../components/form";
+import { fadeOutLeftBig, fadeOutUpBig } from "react-animations";
 
 export const LobbyClosed = (props) => {
   const [isVisibleTitle, setIsVisibleTitle] = useState(true);
   const [isVisibleTitleAnimation, setIsVisibleTitleAnimation] = useState(false);
 
+  const [showResume, setShowResume] = useState(false);
+  const [showResumeAnimation, setShowResumeAnimation] = useState(false);
+
   useEffect(() => {
-    const initialize = async () => {
+    const initializeAnimation = async () => {
       await timeoutPromise(2 * 1000);
       setIsVisibleTitleAnimation(true);
       await timeoutPromise(2 * 1000);
       setIsVisibleTitle(false);
     };
 
-    initialize();
+    initializeAnimation();
   }, []);
 
-  return (
-    <LobbyClosedCss isVisibleTitleAnimation={isVisibleTitleAnimation}>
-      {isVisibleTitle && <div className="title">{props.lobby.game.title}</div>}
+  const initializeTransition = async () => {
+    setShowResumeAnimation(true);
+    await timeoutPromise(2 * 1000);
+    setShowResume(true);
+    setShowResumeAnimation(false);
+  };
 
-      {!isVisibleTitle && (
-        <div className="winners">
-          {props.lobby.winners?.map((winner, index) => (
-            <Winner winner={winner} index={index} key={index} />
-          ))}
+  return !showResume ? (
+    <LobbyClosedCss isVisibleTitleAnimation={isVisibleTitleAnimation} showResumeAnimation={showResumeAnimation}>
+      <>
+        <div className="header">
+          {!isVisibleTitle && (
+            <ButtonBingo variant="primary" margin="10px 10px auto auto" onClick={initializeTransition}>
+              Siguiente
+            </ButtonBingo>
+          )}
         </div>
-      )}
+
+        {isVisibleTitle && <div className="title">{props.lobby.game.title}</div>}
+
+        <div className="winners">
+          {!isVisibleTitle &&
+            props.lobby.winners?.map((winner, index) => <Winner winner={winner} index={index} key={index} />)}
+        </div>
+      </>
     </LobbyClosedCss>
+  ) : (
+    <LobbyResumeCss>
+      <div className="resume">
+        <div className="item">
+          <ButtonAnt variant="contained" color="primary" margin="auto">
+            Volver ap podio
+          </ButtonAnt>
+        </div>
+        <div className="child">
+          <div className="item">asd</div>
+          <div className="item flex">
+            <div className="content-center">
+              ¿Se divirtieron?
+              <ButtonAnt variant="contained" color="success" margin="auto">
+                Jugar de nuevo
+              </ButtonAnt>
+            </div>
+          </div>
+          <div className="item">asd</div>
+          <div className="item">
+            <ButtonAnt variant="contained" color="primary" margin="20px auto" width="80%">
+              Ver reporte completo
+            </ButtonAnt>
+            <ButtonAnt variant="contained" color="primary" margin="20px auto" width="80%">
+              Volver al inicio
+            </ButtonAnt>
+          </div>
+        </div>
+      </div>
+    </LobbyResumeCss>
   );
 };
 
-const fadeOutUpBiganimation = keyframes`${fadeOutUpBig}`;
-
-const LobbyClosedCss = styled.div`
+const LobbyResumeCss = styled.div`
   display: flex;
   height: 100vh;
+  text-align: center;
+
+  .flex {
+    display: flex;
+  }
+
+  .item {
+    border-radius: 10px;
+    background: ${(props) => props.theme.basic.white};
+
+    .content-center {
+      margin: auto;
+    }
+  }
+
+  .resume {
+    width: 90%;
+    margin: auto;
+    display: grid;
+    height: 300px;
+    grid-gap: 20px;
+    grid-template-columns: 1.5fr 3fr;
+
+    .child {
+      display: grid;
+      grid-gap: 20px;
+      grid-template-columns: 1.3fr 1fr;
+      grid-template-rows: 1fr 1fr;
+    }
+  }
+`;
+
+const fadeOutUpBigAnimation = keyframes`${fadeOutUpBig}`;
+const fadeOutLeftBigAnimation = keyframes`${fadeOutLeftBig}`;
+
+const LobbyClosedCss = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 6fr;
+  height: 100vh;
+
+  .header {
+    display: flex;
+  }
 
   .title {
     margin: auto;
@@ -47,12 +136,14 @@ const LobbyClosedCss = styled.div`
     font-size: 1.5rem;
     padding: 10px 10px;
     border-radius: 5px;
-    animation: 2s ${(props) => (props.isVisibleTitleAnimation ? fadeOutUpBiganimation : "")};
+    animation: 2s ${(props) => (props.isVisibleTitleAnimation ? fadeOutUpBigAnimation : "")};
   }
 
   .winners {
+    height: 100%;
     width: 90%;
     margin: auto;
+    animation: 2s ${(props) => (props.showResumeAnimation ? fadeOutLeftBigAnimation : "")};
 
     ${mediaQuery.afterTablet} {
       width: 70vw;
