e8b5e3f9358bdc45a89539b8bb29fbb9fbafcce4
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-10-22 09:50:08 -0500

    ...

--- src/theme/mixins.js
+++ src/theme/mixins.js
@@ -1,8 +1,5 @@
-
-
 export const animatedBackground = ({ backgroundUrl }) => `
   background-image: url("${backgroundUrl}");
-
   background-repeat: repeat;
   background-position: center;
   animation: right-to-left-shift 20s linear infinite alternate;
