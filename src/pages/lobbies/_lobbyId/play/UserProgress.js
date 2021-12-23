ea163e3e90316ff19f950cb20820bce1ae9af389
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-12-23 18:51:59 -0500

    fixing user tabs order and removing unnecessary fetchs

--- src/pages/lobbies/_lobbyId/play/UserProgress.js
+++ src/pages/lobbies/_lobbyId/play/UserProgress.js
@@ -1,40 +1,11 @@
-import React, { useEffect, useState } from "reactn";
+import React, { useState } from "reactn";
 import { firestore } from "../../../../firebase";
 import { darkTheme } from "../../../../theme";
 import { Progress } from "antd";
 
 export const UserProgress = (props) => {
-  const [numberWinners, setNumberWinners] = useState(props.numberWinners);
-  const [userCard, setUserCard] = useState(JSON.parse(props.user.card));
-
-  useEffect(() => {
-    // This useEffect is just to work with auto fill.
-    if (!props.lobby.settings.cardAutofill) return;
-
-    const currentUsers = Object.values(props.lobby.users);
-    const currentUser = currentUsers.find((user) => user.id === props.user.id);
-
-    setNumberWinners(props.numberWinners);
-    setUserCard(JSON.parse(currentUser.card));
-  }, [props.lobby.users, props.numberWinners]);
-
-  useEffect(() => {
-    if (props.lobby?.settings?.cardAutofill) return null;
-
-    const fetchMyWiningCard = () =>
-      firestore
-        .collection("lobbies")
-        .doc(props.lobby.id)
-        .collection("users")
-        .doc(props.user.id)
-        .onSnapshot((userOnShapShot) => {
-          const user = userOnShapShot.data();
-          setNumberWinners(user?.myWinningCard ?? []);
-        });
-
-    const sub = fetchMyWiningCard();
-    return () => sub && sub();
-  }, []);
+  const [numberWinners] = useState(props.user.myWinningCard ?? props.numberWinners);
+  const [userCard] = useState(JSON.parse(props.user.card));
 
   return props.isCard ? (
     <React.Fragment key={numberWinners?.length}>
