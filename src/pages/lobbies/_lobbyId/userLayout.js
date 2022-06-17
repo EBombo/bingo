c712524c3cbe395cd7e40e344ce05c8c6a66b1b6
anargu <anargu@pop-os.localdomain>
2022-06-17 16:25:47 -0500

    implementation invoicing logic

--- src/pages/lobbies/_lobbyId/userLayout.js
+++ src/pages/lobbies/_lobbyId/userLayout.js
@@ -2,7 +2,7 @@ import React, { useGlobal, useState } from "reactn";
 import styled from "styled-components";
 import { Popover, Slider, Spin, Tooltip } from "antd";
 import { Desktop, mediaQuery } from "../../../constants";
-import { config, firestore, firestoreBomboGames, hostName } from "../../../firebase";
+import { config, firebase, firestore, firestoreBomboGames, hostName } from "../../../firebase";
 import { Image } from "../../../components/common/Image";
 import { LoadingOutlined, MessageOutlined } from "@ant-design/icons";
 import { ButtonAnt } from "../../../components/form";
@@ -187,13 +187,21 @@ export const UserLayout = (props) => {
               <div>
                 <div
                   onClick={async () => {
+                    if (props.lobby?.isPlaying) await firestore
+                        .collection("lobbies")
+                        .doc(props.lobby.id)
+                        .collection("users")
+                        .doc(authUser.id)
+                        .update({ hasExited: true });
+
+                    // Reducing counter -1 if is a player.
+                    if (!authUser.isAdmin && props.lobby?.isPlaying) {
+                      await firestore.doc(`lobbies/${props.lobby.id}`).update({
+                        countPlayers: firebase.firestore.FieldValue.increment(-1),
+                      });
+                    }
+
                     props.logout();
-                    await firestore
-                      .collection("lobbies")
-                      .doc(props.lobby.id)
-                      .collection("users")
-                      .doc(authUser.id)
-                      .delete();
                   }}
                   style={{ cursor: "pointer" }}
                 >
