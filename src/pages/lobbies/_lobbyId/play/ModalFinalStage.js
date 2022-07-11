ed79abf77dd82e1bcc5a166d61a4293445649eb9
Pablo <pablo.yncio@immertec.com>
2022-07-11 11:40:12 -0500

    no record

--- src/pages/lobbies/_lobbyId/play/ModalFinalStage.js
+++ src/pages/lobbies/_lobbyId/play/ModalFinalStage.js
@@ -189,7 +189,10 @@ export const ModalFinalStage = (props) => {
                 <ButtonAnt className="btn" color="default" onClick={() => newCards()}>
                   Continuar con cartillas nuevas
                 </ButtonAnt>
-                <ButtonAnt className="btn" color="default" onClick={() => newGame()}>
+                <ButtonAnt className="btn" color="default" onClick={() => {
+                  // TODO: Volver el valor de "myWinningCard" a un array vacio [para todos los usuarios].
+                  newGame();
+                }}>
                   Juego nuevo
                 </ButtonAnt>
                 <ButtonAnt className="btn" color="danger" onClick={() => endGame()}>
